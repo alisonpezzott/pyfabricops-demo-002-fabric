ea67a2f48cb391c6b06bd13e9defc37e6489af76
alisonpezzott <alisonpezzott@gmail.com>
2025-07-20 00:59:40 -0300

    feat: Added function to extract semantic models parameters

--- scripts/deploy_project.py
+++ scripts/deploy_project.py
@@ -62,6 +62,14 @@ export_all_semantic_models(
 )
 
 
+pf.extract_semantic_models_parameters(
+    project_path=project_path,
+    workspace_alias=workspace_alias,
+    config_path=config_path,
+    branch=branch,
+)
+
+
 pf.deploy_all_reports_cicd(
     project_path=project_path,
     workspace_alias=workspace_alias,
